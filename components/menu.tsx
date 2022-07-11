76f916eb4519c84f35120a98667801e2c2f443c9
Felipe <felipetiagorc@yahoo.com.br>
2022-07-11 18:00:24 -0300

    schema

--- components/menu.tsx
+++ components/menu.tsx
@@ -4,6 +4,7 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { Fragment } from 'react';
+import Brasao from './../public/brasao-sp-eleicao.png';
 
 const navigation = [
   { name: 'Inicio', href: '/', current: true },
@@ -19,11 +20,14 @@ export default function NavMenu(user) {
   const { data: session } = useSession();
 
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure
+      as='nav'
+      className='bg-gray-200 h-36  border-b-4 border-b-gray-700 '
+    >
       {({ open }) => (
         <>
-          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
-            <div className='relative flex items-center justify-between h-16'>
+          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 '>
+            <div className='relative flex items-center justify-between h-32 '>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
                 <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
@@ -35,38 +39,41 @@ export default function NavMenu(user) {
                   )}
                 </Disclosure.Button>
               </div>
+              {/* Header-Content-Start */}
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-                <div className='flex-shrink-0 flex items-center'>
-                  <img
-                    className='block lg:hidden h-10 w-auto'
-                    src='https://upload.wikimedia.org/wikipedia/commons/1/1a/Bras%C3%A3o_do_estado_de_S%C3%A3o_Paulo.svg'
-                    alt='BrasaoSP'
-                  />
-                  <img
-                    className='hidden lg:block h-10 w-auto'
-                    src='https://upload.wikimedia.org/wikipedia/commons/1/1a/Bras%C3%A3o_do_estado_de_S%C3%A3o_Paulo.svg'
-                    alt='BrasaoSP'
-                  />
+                {/* Brasao */}
+                <div className='lg:block sm:flex w-auto flex-shrink-0 flex items-center'>
+                  <Image width='140' height='90' src={Brasao} alt='BrasaoSP' />
                 </div>
-                <div className='hidden sm:block sm:ml-6'>
-                  <div className='flex space-x-4'>
-                    {navigation.map(item => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                {/* Navegação */}
+
+                {session?.user?.name ? (
+                  <div className='hidden m-auto sm:block sm:ml-6'>
+                    <div className='flex space-x-4'>
+                      {navigation.map(item => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <p className='my-auto lg:mx-48 md:mx-32 sm:mx-12 text-center font-sans'>
+                    Em atendimento à legislação eleitoral, os demais conteúdos
+                    deste site ficarão indisponíveis de 2 de julho de 2022 até o
+                    final da eleição estadual em São Paulo.
+                  </p>
+                )}
               </div>
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                 <button
