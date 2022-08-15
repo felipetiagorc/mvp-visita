3435101204d7dda66e51b93c284cfb2270d86a87
felipe <felipetiagorc@yahoo.com.br>
2022-08-15 20:15:46 -0300

    testecasa

--- src/repositories/IUsersRepository.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../entities/User";
-
-export interface IUsersRepository{
-    //antes de criar verifica se ja existe
-
-    findByEmail(email: string): Promise<User>
-    save(user: User): Promise<void>;
-}
