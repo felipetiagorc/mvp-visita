496ddaf81db49f8ef69ca7be270fc2fd5374b550
Felipe <felipetiagorc@yahoo.com.br>
2022-10-13 17:45:13 -0300

    erro update

--- pages/api/visitante/upload.js
+++ pages/api/visitante/upload.js
@@ -1,10 +1,41 @@
 import nc from 'next-connect';
-import upload from './src/utils/upload';
+import upload from './../../../utils/upload';
 
-export const handler = nc()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const handler = nc()
   .use(upload.single('file'))
-  .post((req, res) => {
-    const { title, authorName, authorAvatar, imagemUrl } = req.body;
-    const {db} = await conn
-    res.json({});
+  .post(async (req, res) => {
+    const { email } = req.body;
+
+    const usuario = await prisma.user.findFirst({
+      where: { email: email },
+      select: {
+        Documento: true,
+      },
+    });
+
+    await prisma.user.update({
+      where: {
+        email: email,
+      },
+
+      //Argument data.Documento.set of type DocumentoWhereUniqueInput needs exactly one argument, but you provided 0 and 1 and 2 and 3
+      // falta um select aqui ? ou parar de usar Document...
+
+      data: {
+        Documento: {
+          set: [...usuario.Documento, req.file.location],
+        },
+      },
+    });
+    return res.status(200).json({ ok: true });
   });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+export default handler;
--- utils/upload.js
+++ utils/upload.js
@@ -2,6 +2,8 @@ import aws from 'aws-sdk';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 
+import { randomBytes } from 'node:crypto';
+
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_KEY,
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -11,7 +13,7 @@ aws.config.update({
 
 let s3 = new aws.S3({});
 
-export const upload = multer({
+const upload = multer({
   storage: multerS3({
     s3,
     bucket: process.env.AWS_BUCKET,
@@ -20,7 +22,7 @@ export const upload = multer({
       cb(null, { fieldName: file.fieldname });
     },
     key(req, file, cb) {
-      crypto.randomBytes(16, (err, hash) => {
+      randomBytes(16, (err, hash) => {
         if (err) cb(err);
 
         const fileName = `${hash.toString('hex')}-${file.originalname}`;
@@ -29,3 +31,4 @@ export const upload = multer({
     },
   }),
 });
+export default upload;
